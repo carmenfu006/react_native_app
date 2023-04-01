517ae2bc0a57f1e391b8d18f091974260b8a92b7
carmenfu006 <guess_006@hotmail.com>
2023-04-02 00:23:41 +0800

    Create header with logo and profile picture

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { View, SafeAreaView, FlatList, Text } from 'react-native';
+import { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
 
-import { COLORS, NFTData } from '../constants';
-import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
   return (
@@ -13,27 +13,29 @@ const Home = () => {
         <View style={{ zIndex: 0 }}>
           <FlatList
             data={NFTData}
-            renderItem={({ item }) => <NFTCard data={item}/>}
+            renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
             ListHeaderComponent={<HomeHeader />}
           />
         </View>
 
-        <View style={{
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          zIndex: -1
-        }}>
-          <View style={{ height: 300, backgroundColor: COLORS.primary}} />
-          <View style={{ flex: 1, backgroundColor: COLORS.white}} />
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
         </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
